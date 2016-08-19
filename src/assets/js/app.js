54ae3909d7ac41ea5221c89ea9924075eaab740a
John Nixon <jnixon@johndnixon.com>
2016-08-19 14:50:02 -0400

    Build ready
    Added JointsWP style template pieces, google analytics config option,
    google fonts config option, type kit config option, and pagination

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation();
+
+function currentYear() {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("year").innerHTML = n;
+}
+
+currentYear();
+
+// Adds Flex Video to YouTube and Vimeo Embeds
+jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
+	if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
+		jQuery(this).wrap("<div class='widescreen flex-video'/>");
+	} else {
+		jQuery(this).wrap("<div class='flex-video'/>");
+	}
+});
